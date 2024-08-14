c1d4d0704d60fada9e40be49a1f5952d62e341c8
developedbyamin <developedbyamin@gmail.com>
2024-08-14 23:27:41 +0400

    homefeature

--- lib/src/features/home/domain/usecases/pray_time_use_case.dart
+++ lib/src/features/home/domain/usecases/pray_time_use_case.dart
@@ -2,11 +2,17 @@ import 'package:dartz/dartz.dart';
 import 'package:jumma/src/core/usecase/usecase.dart';
 import 'package:jumma/src/features/home/domain/repository/pray_time_repository.dart';
 import '../../../../../service_locator.dart';
+import '../entities/pray_time.dart';
 
 class PrayTimeUseCase extends UseCase<Either,dynamic>{
   @override
   Future<Either> call({params}) async{
     return await sl<PrayTimeRepository>().getPrayTimes();
+  }
+
+
+  DateTime? getNextPrayerTime(PrayTimeEntity prayTimes) {
+    return PrayerTimeCalculator.getNextPrayerTime(DateTime.now(), prayTimes);
   }
 
 }
--- lib/src/features/home/presentation/pages/home.dart
+++ lib/src/features/home/presentation/pages/home.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:jumma/src/core/assets/assets/app_vectors.dart';
+import 'package:jumma/src/core/ads/ad_banner.dart';
 import 'package:jumma/src/features/home/presentation/pages/widgets/home_banner.dart';
+import 'package:jumma/src/features/home/presentation/pages/widgets/pray_time.dart';
 import 'package:jumma/src/features/home/presentation/viewmodel/pray_time_cubit.dart';
 
 class Home extends StatelessWidget {
@@ -34,55 +36,22 @@ class Home extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 HomeBanner(),
+                SizedBox(
+                  height: 16,
+                ),
+                AdBanner(),
+                SizedBox(
+                  height: 16,
+                ),
                 PrayTimeView(),
+                SizedBox(
+                  height: 16,
+                ),
+                AdBanner(),
               ],
             ),
           ),
         ),
-      ),
-    );
-  }
-}
-
-class PrayTimeView extends StatelessWidget {
-  const PrayTimeView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<PrayTimeCubit, PrayTimeState>(
-      builder: (context, state) {
-        if (state is PrayTimeLoading) {
-          return const Center(child: CircularProgressIndicator());
-        } else if (state is PrayTimeSuccess) {
-          final prayTimes = state.prayTimes;
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _buildPrayTimeRow('Fajr', prayTimes.fajr),
-              _buildPrayTimeRow('Dhuhr', prayTimes.dhuhr),
-              _buildPrayTimeRow('Asr', prayTimes.asr),
-              _buildPrayTimeRow('Maghrib', prayTimes.maghrib),
-              _buildPrayTimeRow('Isha', prayTimes.isha),
-            ],
-          );
-        } else if (state is PrayTimeFailure) {
-          return const Center(child: Text('Failed to load prayer times'));
-        } else {
-          return const SizedBox.shrink();
-        }
-      },
-    );
-  }
-
-  Widget _buildPrayTimeRow(String title, String time) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(title),
-          Text(time),
-        ],
       ),
     );
   }
